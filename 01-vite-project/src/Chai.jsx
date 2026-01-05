0e2bb4ce71e9c80e48aa1fa63e11b41cef44abc8
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2026-01-05 18:31:27 +0600

    Add Hook

--- 01-vite-project/src/Chai.jsx
+++ 01-vite-project/src/Chai.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function Chai() {
-    return <h3>Chai Aur Code</h3>;
+  const [counter, setCounter] = useState(15);
+  let Add = () => {
+    setCounter(counter + 1);
+  };
+  let Remove = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h3>Total Number : {counter}</h3>
+      <button onClick={Add}>Add </button>
+      <br />
+      <button onClick={Remove}>Remove</button>
+    </>
+  );
 }
 
 export default Chai;
